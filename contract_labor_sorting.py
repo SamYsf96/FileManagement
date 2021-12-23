8278968af92360a0296d98c2905fcc6eb1f904e6
SamYsf96 <86136070+SamYsf96@users.noreply.github.com>
2021-12-23 13:12:27 -0500

    Update contract_labor_sorting.py

--- contract_labor_sorting.py
+++ contract_labor_sorting.py
@@ -6,27 +6,19 @@ community = ["Alura","ILA","ILBS","ILDB","ILHL","ILK","ILL","ILLWR","ILRP","ILS"
 vendor_names = ["Ascen Workforce","Clipboard Health","Brightstar Care","ATC Healthcare","Maxim Healthcare","AMX Healthcare","AIT Staffing","Elite Medical Staffing","Intelycare"]
 community_qdrive = {'ILA':'Alpharetta', 'Alura':'Alura', 'ILBS':'Bonita Springs', 'ILDB':'Delray', 'ILHL':'Hidden Lakes','IRL':'Ivy Ridge','ILK':'Kenner','ILLWR':'Lakewood Ranch'
                     ,'ILL':'Lewisville', 'ILRP':'Royal Palm', 'ILS':'Sarasota', 'ILSL':'Sugar Land','ILSC':'Sun City','ILT':'Tampa','ILWM':'Windermere'}
-
+date_range = ['12.','11.','10.','09.']
 
 
 def copy_file(file,community_name):
     filedesc = file.split('\\')[-1]
     for i in vendor_names:
-        if i in filedesc and filedesc[-12:-9] == '12.':
-            name = os.path.basename(file)
-            destination = f"Q:\\{community_qdrive[community_name]}\\Executive Director\\Accounting & Finance\\Contract Labor\\12.2021\\Invoices"
-            dest = os.path.join(destination,name)
-            shutil.copyfile(file,dest)
-        elif i in filedesc and filedesc[-12:-9] == '11.':
-            name = os.path.basename(file)
-            destination = f"Q:\\{community_qdrive[community_name]}\\Executive Director\\Accounting & Finance\\Contract Labor\\11.2021\\Invoices"
-            dest = os.path.join(destination,name)
-            shutil.copyfile(file,dest)
-        elif i in filedesc and filedesc[-12:-9] == '10.':
-            name = os.path.basename(file)
-            destination = f"Q:\\{community_qdrive[community_name]}\\Executive Director\\Accounting & Finance\\Contract Labor\\10.2021\\Invoices"
-            dest = os.path.join(destination,name)
-            shutil.copyfile(file,dest)
+        if i in filedesc:
+            for j in date_range:
+                if filedesc[-12:-9] == j:
+                    name = os.path.basename(file)
+                    destination = f"Q:\\{community_qdrive[community_name]}\\Executive Director\\Accounting & Finance\\Contract Labor\\{j}2021\\Invoices"
+                    dest = os.path.join(destination,name)
+                    shutil.copyfile(file,dest)
     
 
 def sort_through_files():
